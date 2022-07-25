4da2aa2dd70e2f0f4ec7567634427db54d2b29da
cykuo <ckuo10@myseneca.ca>
2022-07-24 20:43:49 -0400

    modify main router

--- main.go
+++ main.go
@@ -4,11 +4,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+var temp *template.Template
+
+func init() {
+	temp = template.Must(template.ParseGlob("static/*.html"))
+}
+
+func RunIndex(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "index.html", nil)
+}
+
+func RunIntro(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "intro.html", nil)
+}
+
+func RunHistory(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "history.html", nil)
+}
+
+func RunMechanism(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "mechanism.html", nil)
+}
+
+func RunRef(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "reference.html", nil)
+}
+
+func RunAbout(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "about.html", nil)
+}
+
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
+	http.HandleFunc("/", RunIndex)
+	http.HandleFunc("/intro", RunIntro)
+	http.HandleFunc("/history", RunHistory)
+	http.HandleFunc("/mechanism", RunMechanism)
+	http.HandleFunc("/reference", RunRef)
+	http.HandleFunc("/about", RunAbout)
 
 	fmt.Print("Starting server at port 8000\n")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
